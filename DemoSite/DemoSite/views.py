6c6aa09a66de124dcd964ab0fd980ff0c70febd3
Eilon Klein <eilonlon.klein@gmail.com>
2020-11-24 11:52:53 +0200

    inital commit

---
+++ DemoSite/DemoSite/views.py
@@ -0,0 +1,107 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template, request, redirect, url_for
+from DemoSite import app
+import sqlite3
+from sqlite3 import Error
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Renders the login page."""
+    return render_template(
+        'login.html',
+        title='Login',
+        year=datetime.now().year,
+        message='Login Page.'
+    )
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+        return render_template(
+        'register.html',
+        title='Register',
+        year=datetime.now().year,
+        message='Resgister Page.'
+    )
+
+@app.route('/redirection', methods=['POST'])
+def redirection():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    try:
+        conn = sqlite3.connect(r"database.db")
+        print("Connection is being made!")
+        cursor = conn.cursor()
+        cursor.execute('SELECT EXISTS(SELECT 1 FROM users WHERE username=?);', (username,))
+        
+        if cursor.fetchone()[0] == 0:
+            cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (name, email, username, password))
+            success = True
+        else:
+            print("this user already exists!")
+            success = False
+            return render_template(
+             'error.html',
+            title='Error',
+            year=datetime.now().year,
+            message='Error Page.',
+            error='משתמש עם שם משתמש זהה קיים כבר. אנא נסה שם משתמש אחר.'
+             )
+
+        conn.commit()
+
+    except Error as e:
+        print(e)
+    finally:
+        for row in cursor.execute('SELECT * FROM users ORDER BY name'):
+             print(row)
+        conn.close()
+        if success:
+            return redirect(url_for('profile'))
+
+
+
+@app.route('/profile')
+def profile():
+        return render_template(
+        'profile.html',
+        title='Profile',
+        year=datetime.now().year,
+        message='Profile Page.',
+    )
